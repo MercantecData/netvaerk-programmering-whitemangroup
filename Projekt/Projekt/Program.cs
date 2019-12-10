201bc03f9acd0242a3b9f09aeb2169c37688276e
Sandra Jaworska <sandra.p.jaworska@gmail.com>
2019-12-10 14:44:04 +0100

    Added WIP chat functionality that does not work
    I tried and I failed

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -16,6 +16,7 @@ namespace Projekt
             Console.WriteLine("Choose:");
             Console.WriteLine("1. Client");
             Console.WriteLine("2. Server");
+            Console.WriteLine("3. Chat");
 
             string userInput = Console.ReadLine();
             if (userInput == "1")
@@ -25,6 +26,10 @@ namespace Projekt
             else if (userInput == "2")
             {
                 Server();
+            }
+            else if (userInput == "3")
+            {
+                Chat();
             }
             else
             {
@@ -66,6 +71,37 @@ namespace Projekt
             byte[] buffer = Encoding.UTF8.GetBytes(text);
             stream.Write(buffer, 0, buffer.Length);
             client.Close();
+        }
+
+        static void Chat()
+        {
+            TcpClient client = new TcpClient();
+            Console.WriteLine("Write the port number:");
+            int port = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write the IP address:");
+            IPAddress ip = IPAddress.Parse(Console.ReadLine());
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            client.Connect(endPoint);
+            TcpListener listener = new TcpListener(endPoint);
+            listener.Start();
+            client = listener.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+            if (stream == null) 
+            {
+                Console.WriteLine("Write your message:");
+                string text = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(text);
+                stream.Write(buffer, 0, buffer.Length);
+            } else
+            {
+                byte[] buffer = new byte[256];
+                int numberOfBytesRead = stream.Read(buffer, 0, 256);
+                string message = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+                Console.WriteLine(message);
+                stream.Write(buffer, 0, numberOfBytesRead);
+            }
+            
         }
     }
     
